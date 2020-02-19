7f44f1925b2602f32fc6e13a49b8887153927a44
FabinhoM <fabienmoreira77@gmail.com>
2020-02-19 19:40:59 +0100

    Adding homepage (V1)

---
+++ public/homepage.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>COOKRENTAL - Homepage</title>
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+
+
+<body>
+<div id="wrapper">
+	
+	<div id="logo" class="container">
+		<h1><a href="#">CookRental</a></h1>
+		<p>You are cooked and looking for a car quickly, we are the solution.</p>
+	</div>
+	<div id="page" class="container">
+		<div>
+			<div class="entry">
+				<p>Below is our list of cars available for hire.<br>
+					You will find all the details by clicking on "More details".</p>
+			</div>
+		</div>
+	</div>
+	<div id="three-column" class="container">
+		<div class="tbox1">
+			<div class="box-style box-style01">
+				<div class="content">
+					<div class="image"><img src="images/img.png" width="324" height="200" alt="" /></div>
+					<h2>{marque}{model}</h2>
+					<p>{description}</p>
+					<a href="#" class="button">Details</a>
+				</div>
+			</div>
+		</div>
+		<div class="tbox2">
+			<div class="box-style box-style02">
+				<div class="content">
+					<div class="image"><img src="images/img.png" width="324" height="200" alt="" /></div>
+					<h2>{marque}{model}</h2>
+					<p>{description}</p>
+					<a href="#" class="button">Details</a>
+				</div>
+			</div>
+		</div>
+		<div class="tbox3">
+			<div class="box-style box-style03">
+				<div class="content">
+					<div class="image"><img src="images/img.png" width="324" height="200" alt="" /></div>
+					<h2>{marque}{model}</h2>
+					<p>{description}</p>
+					<a href="#" class="button">Details</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="three-column" class="container">
+		<div class="tbox1">
+			<div class="box-style box-style01">
+				<div class="content">
+					<div class="image"><img src="images/img.png" width="324" height="200" alt="" /></div>
+					<h2>{marque}{model}</h2>
+					<p>{description}</p>
+					<a href="#" class="button">Details</a>
+				</div>
+			</div>
+		</div>
+		<div class="tbox2">
+			<div class="box-style box-style02">
+				<div class="content">
+					<div class="image"><img src="images/img.png" width="324" height="200" alt="" /></div>
+					<h2>{marque}{model}</h2>
+					<p>{description}</p>
+					<a href="#" class="button">Details</a>
+				</div>
+			</div>
+		</div>
+		<div class="tbox3">
+			<div class="box-style box-style03">
+				<div class="content">
+					<div class="image"><img src="images/img.png" width="324" height="200" alt="" /></div>
+					<h2>{marque}{model}</h2>
+					<p>{description}</p>
+					<a href="#" class="button">Details</a>
+				</div>
+			</div>
+		</div>
+	</div>	
+</div>
+<div id="footer">
+	<p>COOKRENTAL - Certification - 2020 - FIPA 2A</a>.</p>
+</div>
+</body>
+</html>
